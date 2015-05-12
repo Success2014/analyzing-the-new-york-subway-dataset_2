a470b42bcc604a783227c71acef8bdbff0e02a23
Success2014 <neogoyeah@gmail.com>
2015-05-12 12:49:11 -0500

    analyze new york subway

---
+++ project.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 20:24:07 2015
+
+@author: Neo
+"""
+
+import numpy as np
+import pandas
+from ggplot import *
+import scipy
+import matplotlib.pyplot as plt
+
+
+"""
+In this question, you need to:
+1) implement the compute_cost() and gradient_descent() procedures
+2) Select features (in the predictions procedure) and make predictions.
+
+"""
+
+def plot_weather_data(turnstile_weather):
+    ''' 
+    plot_weather_data is passed a dataframe called turnstile_weather. 
+    Use turnstile_weather along with ggplot to make another data visualization
+    focused on the MTA and weather data we used in Project 3.
+    
+    Make a type of visualization different than what you did in the previous exercise.
+    Try to use the data in a different way (e.g., if you made a lineplot concerning 
+    ridership and time of day in exercise #1, maybe look at weather and try to make a 
+    histogram in this exercise). Or try to use multiple encodings in your graph if 
+    you didn't in the previous exercise.
+    
+    You should feel free to implement something that we discussed in class 
+    (e.g., scatterplots, line plots, or histograms) or attempt to implement
+    something more advanced if you'd like.
+
+    Here are some suggestions for things to investigate and illustrate:
+     * Ridership by time-of-day or day-of-week
+     * How ridership varies by subway station
+     * Which stations have more exits or entries at different times of day
+       (You can use UNIT as a proxy for subway station.)
+
+    If you'd like to learn more about ggplot and its capabilities, take
+    a look at the documentation at:
+    https://pypi.python.org/pypi/ggplot/
+     
+    You can check out the link 
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+    to see all the columns and data points included in the turnstile_weather 
+    dataframe.
+     
+   However, due to the limitation of our Amazon EC2 server, we are giving you a random
+    subset, about 1/3 of the actual data in the turnstile_weather dataframe.
+    '''
+
+    # your code here
+    plot = ggplot(turnstile_weather, aes(x='hour',y='ENTRIESn_hourly',color='weekday'))+geom_point()+\
+    ggtitle('Number of Entries of Different Hours on Weekdays or Weekends')
+    return plot
+
+
+def compute_r_squared(data, predictions):
+    '''
+    In exercise 5, we calculated the R^2 value for you. But why don't you try and
+    and calculate the R^2 value yourself.
+    
+    Given a list of original data points, and also a list of predicted data points,
+    write a function that will compute and return the coefficient of determination (R^2)
+    for this data.  numpy.mean() and numpy.sum() might both be useful here, but
+    not necessary.
+
+    Documentation about numpy.mean() and numpy.sum() below:
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.sum.html
+    '''
+    
+    # your code here
+    
+    SSE = np.sum((predictions - data)**2)
+    SST = np.sum((np.mean(data)-data)**2)
+    r_squared = 1 - SSE/SST
+    return r_squared
+
+def plot_residuals(turnstile_weather, predictions):
+    '''
+    Using the same methods that we used to plot a histogram of entries
+    per hour for our data, why don't you make a histogram of the residuals
+    (that is, the difference between the original hourly entry data and the predicted values).
+    Try different binwidths for your histogram.
+
+    Based on this residual histogram, do you have any insight into how our model
+    performed?  Reading a bit on this webpage might be useful:
+
+    http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
+    '''
+    
+    plt.figure()
+    (turnstile_weather['''ENTRIESn_hourly'''] - predictions).hist()
+    return plt
+
+def normalize_features(df):
+    """
+    Normalize the features in the data set.
+    """
+    mu = df.mean()
+    sigma = df.std()
+    
+    if (sigma == 0).any():
+        raise Exception("One or more features had the same value for all samples, and thus could " + \
+                         "not be normalized. Please do not include features with only a single value " + \
+                         "in your model.")
+    df_normalized = (df - df.mean()) / df.std()
+
+    return df_normalized, mu, sigma
+
+def compute_cost(features, values, theta):
+    """
+    Compute the cost function given a set of features / values, 
+    and the values for our thetas.
+    
+    This can be the same code as the compute_cost function in the lesson #3 exercises,
+    but feel free to implement your own.
+    """
+    
+    # your code here
+    cost = (sum((np.dot(features, theta) - values)**2))/(2*len(values))
+
+    return cost
+
+def gradient_descent(features, values, theta, alpha, num_iterations):
+    """
+    Perform gradient descent given a data set with an arbitrary number of features.
+    
+    This can be the same gradient descent code as in the lesson #3 exercises,
+    but feel free to implement your own.
+    """
+    
+    m = len(values)
+    cost_history = []
+
+    for i in range(num_iterations):
+        # your code here
+        cost_history.append(compute_cost(features, values, theta))
+        theta = theta - (alpha/m)*np.dot(features.transpose(),(np.dot(features,theta)-values))
+
+    return theta, pandas.Series(cost_history)
+
+def predictions(dataframe):
+    '''
+    The NYC turnstile data is stored in a pandas dataframe called weather_turnstile.
+    Using the information stored in the dataframe, let's predict the ridership of
+    the NYC subway using linear regression with gradient descent.
+    
+    You can download the complete turnstile weather dataframe here:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
+    
+    Your prediction should have a R^2 value of 0.20 or better.
+    You need to experiment using various input features contained in the dataframe. 
+    We recommend that you don't use the EXITSn_hourly feature as an input to the 
+    linear model because we cannot use it as a predictor: we cannot use exits 
+    counts as a way to predict entry counts. 
+    
+    Note: Due to the memory and CPU limitation of our Amazon EC2 instance, we will
+    give you a random subet (~15%) of the data contained in 
+    turnstile_data_master_with_weather.csv. You are encouraged to experiment with 
+    this computer on your own computer, locally. 
+    
+    
+    If you'd like to view a plot of your cost history, uncomment the call to 
+    plot_cost_history below. The slowdown from plotting is significant, so if you 
+    are timing out, the first thing to do is to comment out the plot command again.
+    
+    If you receive a "server has encountered an error" message, that means you are 
+    hitting the 30-second limit that's placed on running your program. Try using a 
+    smaller number for num_iterations if that's the case.
+    
+    If you are using your own algorithm/models, see if you can optimize your code so 
+    that it runs faster.
+    '''
+    # Select Features (try different features!)
+    features = dataframe[['hour','rain','day_week','weekday']]
+    
+    # Add UNIT to features using dummy variables
+    dummy_units = pandas.get_dummies(dataframe['UNIT'], prefix='unit')
+    features = features.join(dummy_units)
+    
+    # Values
+    values = dataframe['ENTRIESn_hourly']
+    m = len(values)
+
+    features, mu, sigma = normalize_features(features)
+    features['ones'] = np.ones(m) # Add a column of 1s (y intercept)
+    
+    # Convert features and values to numpy arrays
+    features_array = np.array(features)
+    values_array = np.array(values)
+
+    # Set values for alpha, number of iterations.
+    alpha = 0.1 # please feel free to change this value
+    num_iterations = 75 # please feel free to change this value
+
+    # Initialize theta, perform gradient descent
+    theta_gradient_descent = np.zeros(len(features.columns))
+    theta_gradient_descent, cost_history = gradient_descent(features_array, 
+                                                            values_array, 
+                                                            theta_gradient_descent, 
+                                                            alpha, 
+                                                            num_iterations)
+    
+    plot = None
+    # -------------------------------------------------
+    # Uncomment the next line to see your cost history
+    # -------------------------------------------------
+    # plot = plot_cost_history(alpha, cost_history)
+    # 
+    # Please note, there is a possibility that plotting
+    # this in addition to your calculation will exceed 
+    # the 30 second limit on the compute servers.
+    
+    predictions = np.dot(features_array, theta_gradient_descent)
+    return predictions, plot, theta_gradient_descent,values_array
+
+
+def plot_cost_history(alpha, cost_history):
+   """This function is for viewing the plot of your cost history.
+   You can run it by uncommenting this
+
+       plot_cost_history(alpha, cost_history) 
+
+   call in predictions.
+   
+   If you want to run this locally, you should print the return value
+   from this function.
+   """
+   cost_df = pandas.DataFrame({
+      'Cost_History': cost_history,
+      'Iteration': range(len(cost_history))
+   })
+   return ggplot(cost_df, aes('Iteration', 'Cost_History')) + \
+      geom_point() + ggtitle('Cost History for alpha = %.3f' % alpha )
+
+
+#turnstile_weather = pandas.read_csv("turnstile_weather_v2.csv")
+#print plot_weather_data(turnstile_weather)
+
+
+df = pandas.read_csv("turnstile_weather_v2.csv")
+pred, pp, theta, value = predictions(df)
+
+print compute_r_squared(value, pred)
+
